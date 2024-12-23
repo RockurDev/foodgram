aac84ff778b094f9e60cb7b4749b087a9f497540
Daniil Tropin <Rockur@ya.ru>
2024-12-23 22:44:07 +0300

    feat: Add avatar preview to admin panel, switch to UserAdmin to allow password change

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,16 +1,42 @@
-from django.contrib import admin
+from typing import Literal, Union
 
-from users.models import User, UserSubscriptions
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.safestring import SafeText
+
+from users.models import UserSubscriptions
+
+User = get_user_model()
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     """
     Admin panel settings for the User model.
     """
 
-    list_display = ('username', 'email', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': ('avatar_preview', 'username', 'password')}),
+        *UserAdmin.fieldsets[1:],
+    )
+
+    list_display = (
+        'id',
+        'avatar_preview',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
     search_fields = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('avatar_preview',)
+
+    def avatar_preview(self, obj) -> Union[SafeText, Literal['']]:
+        return obj.avatar_preview
+
+    avatar_preview.short_description = 'Аватар'
 
 
 @admin.register(UserSubscriptions)
@@ -21,3 +47,6 @@ class UserSubscriptionsAdmin(admin.ModelAdmin):
 
     list_display = ('subscriber', 'subscribed_to')
     search_fields = ('subscriber__username', 'subscribed_to__username')
+
+
+admin.site.unregister(Group)
