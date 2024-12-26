aa9a3b751982c9753fdf70c8310ec7eff1bfb49b
Daniil Tropin <Rockur@ya.ru>
2024-12-26 04:04:11 +0300

    Now constants in settings.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,19 +1,14 @@
+from django.conf import settings
 from rest_framework.pagination import PageNumberPagination
 
-from .constants import (
-    PAGINATION_MAX_PAGE_SIZE,
-    PAGINATION_PAGE_SIZE,
-    PAGINATION_PAGE_SIZE_QUERY_PARAM,
-)
 
-
-class DefaultPagintaion(PageNumberPagination):
+class DefaultPagination(PageNumberPagination):
     """
     Custom pagination class to set a default page size
     and allow dynamic page size
     through a query parameter with a maximum limit.
     """
 
-    page_size = PAGINATION_PAGE_SIZE
-    page_size_query_param = PAGINATION_PAGE_SIZE_QUERY_PARAM
-    max_page_size = PAGINATION_MAX_PAGE_SIZE
+    page_size = settings.PAGINATION_PAGE_SIZE
+    page_size_query_param = settings.PAGINATION_PAGE_SIZE_QUERY_PARAM
+    max_page_size = settings.PAGINATION_MAX_PAGE_SIZE
