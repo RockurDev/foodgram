87e37e54260d09276ce61ea2ae12f8032b9c8f3d
Daniil Tropin <Rockur@ya.ru>
2024-12-26 04:08:09 +0300

    Remove related names, add validators constants

--- backend/recipes/constants.py
+++ backend/recipes/constants.py
@@ -9,19 +9,7 @@ MAX_RECIPE_NAME_LENGTH = 256
 
 MAX_LINK_LENGTH = 8
 
-# Related names
-RECIPES_WITH_TAG = 'recipes_with_tag'
-RECIPES_USING_TAG = 'recipes_using_tag'
-TAGS_OF_RECIPE = 'tags_of_recipe'
-
-INGREDIENTS_IN_RECIPE = 'ingredients_in_recipe'
-RECIPES_USING_INGREDIENT = 'recipes_using_ingredient'
-RECIPES_WITH_INGREDIENT = 'recipes_with_ingredient'
-
-AUTHORED_RECIPES = 'authored_recipes'
-
-SHOPPING_CART = 'shopping_cart'
-SHOPPING_CARTS_WITH_RECIPE = 'shopping_carts_with_recipe'
-
-FAVORITES = 'favorites'
-FAVORITED_BY_USERS = 'favorited_by_users'
+MIN_COOKING_TIME = 1
+MIN_AMOUNT = 1
+MAX_COOKING_TIME = 60 * 24
+MAX_AMOUNT = 10000
