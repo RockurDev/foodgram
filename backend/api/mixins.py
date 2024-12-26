9a954b92db56c6d302edf790778f9f1b5cb37622
Daniil Tropin <Rockur@ya.ru>
2024-12-26 04:03:02 +0300

    Now library decodes images

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -1,115 +1,14 @@
-import base64
-import binascii
 import io
-import uuid
-from collections import Counter
-from typing import Optional
 
-from django.core.files.base import ContentFile
+from django.db.models import F, Sum
 from django.http import FileResponse
-
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
-from rest_framework import serializers
 from rest_framework.request import Request
 
-from recipes.models import Recipe
-from users.models import User
-
-
-class Base64DecoderMixin:
-    """Provides functionality to decode a base64-encoded string."""
-
-    def decode_string(self, encoded_string: str, field_name: str) -> bytes:
-        if (
-            not isinstance(encoded_string, str)
-            or ';base64,' not in encoded_string
-        ):
-            raise serializers.ValidationError(
-                {field_name: 'Invalid base64 string format.'}
-            )
-        _, base64_str = encoded_string.split(';base64,')
-        try:
-            return base64.b64decode(base64_str)
-        except (binascii.Error, ValueError):
-            raise serializers.ValidationError(
-                {field_name: 'The base64 string is corrupted or invalid.'}
-            )
-
-
-class ImageDecoderMixin(Base64DecoderMixin):
-    """
-    Provides functionality to decode base64-encoded images
-    into ContentFile objects.
-    """
-
-    def get_image_file(
-        self, encoded_string: str, field_name: str = 'image'
-    ) -> ContentFile:
-        decoded_data = self.decode_string(encoded_string, field_name)
-        mime_type, _ = encoded_string.split(';', 1)
-        file_extension = mime_type.split('/')[-1]
-        file_name = f'{uuid.uuid4()}.{file_extension}'
-        return ContentFile(decoded_data, name=file_name)
-
-
-class RecipesDataMixin:
-    """Provides utility methods for retrieving recipes data for a user."""
-
-    def get_recipes_count(self, obj) -> int:
-        return obj.subscribed_to.authored_recipes.count()
-
-    def get_recipes(self, obj: Recipe):
-        from api.serializers import ShortRecipeSerializer
-
-        request = self.context.get('request')  # type: ignore
-
-        recipes_limit = (
-            request.query_params.get('recipes_limit') if request else None
-        )
-        if recipes_limit:
-            try:
-                recipes_limit = int(recipes_limit)
-            except ValueError:
-                recipes_limit = None
-
-        queryset = obj.subscribed_to.authored_recipes.all()  # type: ignore
-        recipes = queryset[:recipes_limit] if recipes_limit else queryset
-
-        return ShortRecipeSerializer(recipes, many=True).data
-
-
-class UserDataMixin:
-    """Provides utility methods for retrieving user data."""
-
-    def get_user_data(self, user) -> dict:
-        request = self.context.get('request')  # type: ignore
-        return {
-            'id': user.id,
-            'username': user.username,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'email': user.email,
-            'is_subscribed': self.is_user_subscribed(user),
-            'avatar': self.get_user_avatar(user, request),
-        }
-
-    def is_user_subscribed(self, user) -> bool:
-        from users.models import UserSubscriptions
-
-        request = self.context.get('request')  # type: ignore
-        if not request or not request.user.is_authenticated:
-            return False
-        return UserSubscriptions.objects.filter(
-            subscriber=request.user, subscribed_to=user
-        ).exists()
-
-    def get_user_avatar(
-        self, user: User, request: Optional[Request]
-    ) -> Optional[str]:
-        return user.avatar.url if user.avatar else None
+from recipes.models import IngredientRecipe
 
 
 class ShoppingListGeneratorMixin:
@@ -151,26 +50,39 @@ class ShoppingListGeneratorMixin:
         )
 
     def _get_shopping_cart_text(self, user) -> str:
-        recipes = self.request.user.shopping_cart.all()  # type: ignore
-        ingredients_counter = Counter()
+        # Query ingredients and their total amounts
+        # for the user's shopping cart
+        ingredient_amount_queryset = (
+            IngredientRecipe.objects.filter(
+                recipes__shopping_carts_with_recipe__user=user
+            )
+            .values('ingredient__name')
+            .annotate(total_amount=Sum(F('amount')))
+            .order_by('ingredient__name')
+        )
 
-        # Collect and count ingredients across all recipes
-        for recipe in recipes:
-            for ingredient in recipe.recipe.ingredients.all():
-                ingredients_counter[ingredient.name] += 1
-
-        # Format the ingredients and recipes list
         shopping_cart = []
-        for recipe in recipes:
-            name = recipe.recipe.name
-            cooking_time = recipe.recipe.cooking_time
-            recipe_info = (
-                f'Рецепт: {name} | Время приготовления: {cooking_time} мин'
+
+        # Iterate over recipes in the user's shopping cart
+        for recipe in user.shopping_cart.values(
+            'recipe__name', 'recipe__cooking_time'
+        ):
+            recipe_name = recipe['recipe__name']
+            cooking_time = recipe['recipe__cooking_time']
+            recipe_info = ' | '.join(
+                (
+                    f'Рецепт: {recipe_name}',
+                    f'Время приготовления: {cooking_time} мин',
+                )
             )
             shopping_cart.append(recipe_info)
 
-        shopping_cart.append('\nИнгредиенты: \n')
-        for ingredient, count in ingredients_counter.items():
-            shopping_cart.append(f'{ingredient}: {count}')
+        shopping_cart.append('\nИнгредиенты:\n')
+
+        # Iterate over ingredient queryset and format output
+        for ingredient_entry in ingredient_amount_queryset:
+            ingredient = ingredient_entry['ingredient__name']
+            total_amount = ingredient_entry['total_amount']
+            shopping_cart.append(f'{ingredient}: {total_amount}')
 
         return '\n'.join(shopping_cart)
