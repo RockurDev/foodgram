b812ef2b70c627124c490c293a83123a2e28823a
Daniil Tropin <Rockur@ya.ru>
2024-12-30 02:32:32 +0300

    Remove 's/' to recipes app

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('s/', include('recipes.urls')),
+    path('', include('recipes.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
