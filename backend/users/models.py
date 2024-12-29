26e7dbe7f8d40965fbb420a63ce5b92904ef9b4b
Daniil Tropin <Rockur@ya.ru>
2024-12-29 21:17:33 +0300

    Now constants in constants.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,10 @@
-from typing import Literal, Union
-
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import F, Q
-from django.utils.safestring import SafeText, mark_safe
 from django.utils.translation import gettext_lazy as _
 
+from .constants import MAX_USER_FIRST_NAME_LENGTH, MAX_USER_LAST_NAME_LENGTH
 from .utils import get_avatar_path
 
 
@@ -14,8 +12,12 @@ class User(AbstractUser):
     """Custom user model with an additional avatar field."""
 
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=150)
-    last_name = models.CharField(_('last name'), max_length=150)
+    first_name = models.CharField(
+        _('first name'), max_length=MAX_USER_FIRST_NAME_LENGTH
+    )
+    last_name = models.CharField(
+        _('last name'), max_length=MAX_USER_LAST_NAME_LENGTH
+    )
     avatar = models.ImageField(
         upload_to=get_avatar_path,
         default=None,
@@ -30,14 +32,6 @@ class User(AbstractUser):
         ordering = ('id',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-
-    @property
-    def avatar_preview(self) -> Union[SafeText, Literal['']]:
-        if self.avatar:
-            return mark_safe(
-                f'<img src={self.avatar.url} width="90" height="90" />'
-            )
-        return ''
 
     def __str__(self) -> str:
         return self.username
