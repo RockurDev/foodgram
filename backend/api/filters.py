8cf4c70665231229a6ab036e3400bd4264a140ec
Daniil Tropin <Rockur@ya.ru>
2024-12-31 01:17:59 +0300

    Remove extra filter method. FilterSet works using related name in field_name as queryset

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AnonymousUser
 from django_filters import rest_framework as filters
 
 from recipes.models import Recipe, Tag
@@ -15,22 +14,11 @@ class RecipeFilter(filters.FilterSet):
         queryset=Tag.objects.all(),
         to_field_name='slug',
     )
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    is_favorited = filters.BooleanFilter(
-        field_name='favorited_by_users', method='filter_by_user_related_field'
-    )
+    is_favorited = filters.BooleanFilter(field_name='favorites__user')
     is_in_shopping_cart = filters.BooleanFilter(
-        field_name='shopping_carts_with_recipe',
-        method='filter_by_user_related_field',
+        field_name='shopping_cart__user'
     )
 
     class Meta:
         model = Recipe
-        fields = ('tags',)
-
-    def filter_by_user_related_field(self, queryset, name, value):
-        user = self.request.user  # type: ignore
-        if value and not isinstance(user, AnonymousUser):
-            filter_kwargs = {f'{name}__user': user}
-            return queryset.filter(**filter_kwargs)
-        return queryset
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
