d20d92839fc3d4fff8867eb495987db300706655
Daniil Tropin <Rockur@ya.ru>
2024-12-31 15:25:03 +0300

    Remove router version

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,14 +3,14 @@ from rest_framework.routers import DefaultRouter
 
 from .views import IngredientsViewset, RecipeViewset, TagViewset, UserViewset
 
-router_V1 = DefaultRouter()
+router = DefaultRouter()
 
-router_V1.register('users', UserViewset, basename='users')
-router_V1.register('tags', TagViewset, basename='tags')
-router_V1.register('recipes', RecipeViewset, basename='recipes')
-router_V1.register('ingredients', IngredientsViewset, basename='ingredients')
+router.register('users', UserViewset, basename='users')
+router.register('tags', TagViewset, basename='tags')
+router.register('recipes', RecipeViewset, basename='recipes')
+router.register('ingredients', IngredientsViewset, basename='ingredients')
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
-    path('', include(router_V1.urls)),
+    path('', include(router.urls)),
 ]
